491548a260078fc517ee7310e14f30a439a1c97e
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2022-07-12 12:08:49 +0530

    code base commit

---
+++ Card2cash.in/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Card2cashin
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ Card2cash.in/Controllers/ServicesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Card2cashin.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View("CreditCardToCash");
+        }
+        public ActionResult CreditCardToCash()
+        {
+            return View("CreditCardToCash");
+        }
+
+        public ActionResult InstantCashOnCreditCard()
+        {
+            return View("InstantCashOnCreditCard");
+        }
+        public ActionResult SpotCashOnCreditCard()
+        {
+            return View("SpotCashOnCreditCard");
+        }
+
+    }
+}
