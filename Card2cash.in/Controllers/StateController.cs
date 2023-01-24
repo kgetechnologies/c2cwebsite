0a104403abb7b7580d629dabfb465bd810bbd36b
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2023-01-24 07:49:57 +0530

    Fixes

--- Card2cash.in/Controllers/StateController.cs
+++ Card2cash.in/Controllers/StateController.cs
@@ -40,7 +40,7 @@ namespace Card2cashin.Controllers
 			ViewBag.DisplayName = stateName?.Replace("-", " ");
 			ViewBag.LinkValue = stateName?.Replace(" ", "-");
 			ViewBag.Id = Helper.GetIdByName(stateName);
-			ViewBag.CanonicalUri = "credit-card-to-cash-in-" + ViewBag.LinkValue;
+			ViewBag.CanonicalUri = ("credit-card-to-cash-in-" + stateName?.Replace(" ", "-")).ToLower();
 
 			ViewBag.desc = string.Format("cash for credit card in {0} | Credit card to Cash in {0}, Cheap card to cash service in {0},credit card to instant cash in {0},credit card to Spot cash in {0}", ViewBag.DisplayName);
 			ViewBag.Title = string.Format("cash for credit card in {0} | Credit Card to Cash in {0} | Spot Cash on Credit Card in {0}", ViewBag.DisplayName);
